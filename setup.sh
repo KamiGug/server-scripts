98b222b0342641f8520409da9b91e5d7b6762aa8
test <test@test.test>
2025-01-29 10:45:27 +0000

    feat: add -y to apt commands

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ do
 	esac
 done
 
-sudo apt update && sudo apt -y upgrade;
-sudo apt-get install ca-certificates curl;
+sudo apt update && sudo apt upgrade -y;
+sudo apt-get install -y ca-certificates curl;
 sudo install -m 0755 -d /etc/apt/keyrings;
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc;
 sudo chmod a+r /etc/apt/keyrings/docker.asc;
@@ -19,7 +19,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
 sudo apt-get update;
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras;
-sudo apt install uidmap;
+sudo apt install -y uidmap;
 sudo useradd $dockeruser;
 echo '$dockeruser:$password' | sudo chpasswd
 sudo usermod -a -G docker $dockeruser;
