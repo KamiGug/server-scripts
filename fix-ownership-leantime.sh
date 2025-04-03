bb498ebb5d9610dd2b646ba65bfd23123bfe9a90
kamigug <gug.kamil@gmail.com>
2025-04-03 09:01:04 +0200

    feat: add permission fixing script for leantime

---
+++ fix-ownership-leantime.sh
@@ -0,0 +1,4 @@
+docker exec --user=root kanban "/bin/sh -c 'chown -R www-data:www-data /var/www/html/public/userfiles'"
+docker exec --user=root kanban "/bin/sh -c 'chown -R www-data:www-data /var/www/html/userfiles'"
+docker exec --user=root kanban "/bin/sh -c 'chown -R www-data:www-data /var/www/html/app/Plugins'"
+docker exec --user=root kanban "/bin/sh -c 'chown -R www-data:www-data /var/www/html/storage/logs'"
